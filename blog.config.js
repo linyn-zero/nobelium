134fc4c3a176ed85217da8fafe7681c81c2dd9d0
Xin Huang <112923198+linyn-zero@users.noreply.github.com>
2025-05-04 17:49:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  title: 'Linyn\'s 私厨小屋',
+  author: 'Linyn',
+  email: 'linyn_zero@foxmail.com',
+  link: 'https://lime-manuscript-ede.notion.site/1e90dc9336518096aa85c0b0d1e61307?v=1e90dc93365181028810000cd079e251',
+  description: 'Linyn\'s awesome website.',
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
